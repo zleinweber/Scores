3883af49c63ba2f9e1fa333b38b3f4d6c529c15b
Zachary <zleinweb@gmail.com>
2014-01-16 15:19:19 -0600

    Working first version of this program.
    The program will now take all needed input stats and then output its guess at the games outcome.
    
    Currently everything runs, more or less, without asking. This will change.
    Also, I would like to add a Tk interface.

--- Zach_Scores.py
+++ Zach_Scores.py
@@ -1,11 +1,24 @@
+# This is a program requested by Butch Miller.
+# It takes input in the form of a few football team stats.
+# Then it outputs a score guess based on a calculation Butch provided.
+
 from os import system as sy
 
+# Currently, the entire app is contained in this one class.
+# I haven't found any reason to use regular functions instead or
+# add any additional classes.  I would also like to add a Tk interface going forward.
 class Scores_App(object):
 
+	# Currently init defines the important instance variables
+	# Then automatically runs the method for populating them
 	def __init__(self):
+		# These two varialbes are strings containing team name
 		self.HName = raw_input('Home Team Name?> ')
 		self.VName = raw_input('Visitor Team Name?> ')
+		# This is an index for team stats
 		self.Stats = ['Points', 'Points Allowed', 'Games Played']
+		# Two dictoinaries that will get populated with the actual stats
+		# Related to the home team and away team.
 		self.Home = {
 			'Points': 0,
 			'Points Allowed': 0,
@@ -18,14 +31,25 @@ class Scores_App(object):
 			'Games Played': 0,
 			'AVGP': 0,
 			'AVGPA': 0}
+		# Init auto runs the method to populate the stats.  This will change I think.
 		self.stat_fill()
+		# This is just a placeholder to make sure everything populated as it should.
 		print self.Home
+		# This autoruns the method to calculate 'guessed score'.
+		# This will require user input in the future.
 		self.Home_Score()
 
+	# Stat population is broke into two methods.
+	# This one actually asks the user to input the stat.
+	# This is seperate from Stat_Fill so that the except clause can begin
+	# On the same stat where the exception occured.
 	def stat_ask(self, statvar):
+		# This variable contains the key to be used for each teams stat value.
 		curstat = statvar
+		# Try except takes input as raw_input then converts it to int
+		# If that fails then it ouputs the error and tries again.
 		try:
-			sy('clear')
+			sy('clear') # Clears Screen. Bear in mind. This won't work in Windows.
 			print 'Home Team %s?' % curstat
 			self.Home[curstat] = int(raw_input('> '))
 			print 'Visitor %s?' % curstat
@@ -34,15 +58,25 @@ class Scores_App(object):
 			print 'Type a Number!'
 			self.stat_ask(curstat)
 
+	# This method populates teams variables with the appropriate stat.
+	# For variables that require user input it invokes stat_ask to get it.
+	# For the rest it calculates it (The rest are averages).
 	def stat_fill(self):
+		# Scans through the stats using index points.
 		n = 0
 		for item in self.Stats:
+			# Ask user to input the stat.
+			# self.Stats[n] refers to the key currently in use.
 			self.stat_ask(self.Stats[n])
-			n+= 1
+			# Add one to go to the next index point.
+			n += 1
+		# Calculates the remaining averages from the user input stats.
 		self.Home['AVGP'] = self.Home['Points'] / self.Home['Games Played']
 		self.Home['AVGPA'] = self.Home['Points Allowed'] / self.Home['Games Played']
 		
 
+	# This method is the one that actually uses Butch's calculation.
+	# There are two different calculations that are condition based.
 	def Home_Score(self):
 		
 		self.home_score = ((self.Home['AVGP'] - self.Visitor['AVGPA']) / 2) + self.Visitor['AVGPA'] + 3
@@ -50,7 +84,6 @@ class Scores_App(object):
 		print self.home_score
 
 
-## home_score_against HPA / 17
 
 app = Scores_App()
 
